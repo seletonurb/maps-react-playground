1ccde20facc3c873eda8108331b6eec7af8db528
Bruno Teles <seletonurb@gmail.com>
2022-08-11 09:02:03 -0300

    Upgraded to React v18

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './index.scss';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store, history } from './store';
 import { ConnectedRouter } from 'connected-react-router';
 
 import App from './components/App';
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container);
+root.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
-  </Provider>,
-
-  document.getElementById('root')
+  </Provider>
 );
